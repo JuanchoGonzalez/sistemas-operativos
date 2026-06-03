1498431d73b0cf151bc2570352fc75e2e16e24f0
JuanchoGonzalez <juancruzg30@gmail.com>
2026-06-03 15:51:24 -0300

    skip_task_instruccion

--- kernel-juguete/05-kernel-tasks/trap.c
+++ kernel-juguete/05-kernel-tasks/trap.c
@@ -37,6 +37,7 @@ void trap(struct trap_frame *tf)
             if (task) {
                 printf("Task %s in CPU %d illegal instruction at address %x\n",
                        task->name, cpu_id, ra);
+                
                 skip_task_instruction(tf);
                 ra = tf->ra;
             } else
